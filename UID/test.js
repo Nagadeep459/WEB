7fa5332889f6a5e6442b994412ca5586e74b1cca
Nagadeep <143193221+Nagadeep459@users.noreply.github.com>
2025-05-01 16:15:23 +0530

    Reuploading all

---
+++ UID/test.js
@@ -0,0 +1,7 @@
+console.log("NAGA")
+const accountId = 23211
+var accountEmail = "naga21@gmail.com"
+console.table([accountId, accountEmail])
+
+console.log(typeof "NAGA");
+console.log(typeof accountId);
